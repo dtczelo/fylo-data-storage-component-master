32079f81cfebe811730a30446c11a0813e9f753c
Guillaume Laloi <dtczelo@gmail.com>
2021-03-10 14:06:30 +0100

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+let signature = document.getElementById("signature");
+
+window.onload = function () {
+    document.getElementById("loadBar").classList.add("onload");
+    counter();
+};
+
+function counter() {
+    const count = document.getElementById("count");
+    let number = 0;
+    let inc = function () {
+        number === 185 ? clearInterval() : count.innerHTML = (number += 1) + "<span> GB LEFT</span>";
+    };
+    setInterval(() => inc(), 10)
+}
+
+signature.addEventListener("click", () => {
+    signature.classList.toggle("signature-active")
+});
